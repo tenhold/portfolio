70cfa3340f95fba993f0a8f896ad6f6d24b7ef32
tenhold <tenhold@gmail.com>
2021-01-30 14:50:23 -0600

    (add) favicon to site

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html')
+      template: path.join(__dirname, 'src', 'index.html'),
+      favicon: path.join(__dirname, 'src', 'favicon.ico')
     })
   ]
 }
